05038df57b5d981726c8d134c242d7e811be9d1b
webtoma <thomas.delalbre@gmail.com>
2023-10-02 14:36:44 +0200

    updating JS files iwth corrections

--- src/js/main.js
+++ src/js/main.js
@@ -1,87 +1,105 @@
-/* Comment on top of my file */
+fetch('https://dummyjson.com/products?limit=24')
+.then(response => response.json())
+.then(response => {
+    results = response.products
+    resultsDiv = "";
+    for(let i = 0; i < results.length; i++){
+       //console.log(results[i]);
+       let starsNotation = getStars(results[i].rating);
+       let backgroundColor = getBackgroundPrice(results[i].price);
+       resultsDiv += `
+       <div class="col mb-5">
+           <div class="card ${backgroundColor} h-100">
+               <img class="card-img-top"
+                   src="${results[i].images[0]}"
+                   alt="...">
+               <!-- Product details-->
+               <div class="card-body p-4">
+                   <div class="text-center">
+                       <!-- Product name-->
+                       <h6 class="fw-bolder">${results[i].brand}</h6>
+                       <h5 class="fw-bolder">${results[i].title}</h5>
+                       <h6 class="fw-bolder">${starsNotation}</h6>
+                       <!-- Product price-->
+                       ${results[i].price} €
+                   </div>
+               </div>
+               <!-- Product actions-->
+               <div class="card-footer p-4 pt-0 border-top-0 bg-transparent">
+                   <div class="text-center">
+                       <a class="btn btn-outline-dark mt-auto" href="single-product.html">View Product</a>
+                   </div>
+               </div>
+           </div>
+       </div>
+       `;
+    }
+    injectInHTML('products_list', resultsDiv);
+})
+.catch(console.error);
 
-function getFormattedProducts(products){
-    let productsDisplay = ''
-    products.forEach(product => {
-        productsDisplay += `
-        <div class="col mb-5">
-            <div class="card h-100">
-                <!-- Product image-->
-                <img class="card-img-top"
-                    src="${product.images[0]}"
-                    alt="...">
-                <!-- Product details-->
-                <div class="card-body p-4">
-                    <div class="text-center">
-                        <!-- Product name-->
-                        <h6 class="fw-bolder">${product.brand}</h6>
-                        <h5 class="fw-bolder">${product.title}</h5>
-                        <!-- Product price-->
-                        ${product.price}
-                    </div>
-                </div>
-                <!-- Product actions-->
-                <div class="card-footer p-4 pt-0 border-top-0 bg-transparent">
-                    <div class="text-center">
-                        <a class="btn btn-outline-dark mt-auto" href="single-product.html">View Product</a>
-                    </div>
-                </div>
-            </div>
-        </div>
-        `
-    })
-    return productsDisplay;
-}
-function getFormattedPosts(posts){
-    let postsDisplay = ''
-    posts.forEach(post => {
-        postsDisplay += `
+function displayPosts(posts){
+    let postsDiv = "";
+    for( let i = 0; i< posts.length; i++){
+        post = posts[i];
+        postsDiv += `
         <div class="col-lg-6">
-            <div class="card mb-4">
-                <a href="#!"><img class="card-img-top"
-                        src="https://dummyimage.com/700x350/dee2e6/6c757d.jpg"
-                        alt="..."></a>
-                <div class="card-body">
-                    <div class="small text-muted">${post.tags[0]}</div>
-                    <h2 class="card-title h4">${post.title}</h2>
-                    <p class="card-text">${post.body}</p>
-                    <a class="btn btn-primary" href="single-post.html">Read more →</a>
-                </div>
+        <div class="card mb-4">
+            <a href="#!"><img class="card-img-top"
+                    src="https://dummyimage.com/700x350/dee2e6/6c757d.jpg"
+                    alt="..."></a>
+            <div class="card-body">
+                <div class="small text-muted">${post.tags[0]}</div>
+                <h2 class="card-title h4">${post.title}</h2>
+                <p class="card-text">${post.body}</p>
+                <a class="btn btn-primary" href="single-post.html">Read more →</a>
             </div>
         </div>
-        `
-    })
-    return postsDisplay;
+    </div>
+        `;
+    }
+    injectInHTML('posts_list', postsDiv);
 }
-
-
-
-function displayProducts(){
-    
-    let products = []
-    products = fetch('https://dummyjson.com/products', {
-        method: 'GET'
-    })
+function injectInHTML(target, htmlData){
+    document.getElementById(target).innerHTML = htmlData
+}
+function getPosts(){
+    fetch('https://dummyjson.com/posts')
     .then(response => response.json())
-    .then(data => {
-        productsDiv = getFormattedProducts(data.products)
-        document.getElementById('products_list').innerHTML = productsDiv
+    .then(response => {
+            displayPosts(response.posts)
     })
-    .catch(error => console.log(error))
+    .catch(console.error);
 }
-function displayPosts(){
-    let posts = [];
-    posts = fetch('https://dummyjson.com/posts', {
-        method: 'GET'
-    })
-    .then(response => response.json())
-    .then(data => {
-        postsDiv = getFormattedPosts(data.posts)
-        document.getElementById('posts_list').innerHTML = postsDiv
-    })
-    .catch(error => console.log(error))
+function getBackgroundPrice(price){
+    if(price <= 100){
+        backgroundColor = "bg-dark text-light";
+    }else{
+         backgroundColor = "bg-light";
+    }
+    return backgroundColor;
+}
+function getStars(rating){
+    let starsNotation = "";
+       if(rating >= 1 && rating < 2){
+              starsNotation += `⭐`;
+        }else if(rating >= 2 && rating < 3){
+                starsNotation += `⭐⭐`;
+        }else if(  rating >= 3 && rating < 4){
+                starsNotation += `⭐⭐⭐`;
+        }else if(  rating >= 4 && rating < 5){
+                starsNotation += `⭐⭐⭐⭐`;
+        }else if(  rating == 5){
+                starsNotation += `⭐⭐⭐⭐⭐`;
+        }
+    return starsNotation;
 }
 
 
-displayProducts();
-displayPosts();
+
+
+getPosts();
+
+
+
+
